44a08d24520092259890feebc13826e92f7b6f51
usetoconfuse <toby.tischler@gmail.com>
2025-05-17 23:20:08 +0100

    Restyled top app bar

--- app/src/main/java/com/example/climbing_app/ui/components/CommonComponents.kt
+++ app/src/main/java/com/example/climbing_app/ui/components/CommonComponents.kt
@@ -12,13 +12,13 @@ import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.filled.Star
-import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -37,7 +37,7 @@ import com.example.climbing_app.data.ClimbTagStyle
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ClimbingTopAppBar(title: String) {
-    CenterAlignedTopAppBar(
+    TopAppBar(
         colors = TopAppBarDefaults.topAppBarColors(
             containerColor = MaterialTheme.colorScheme.primaryContainer
         ),
