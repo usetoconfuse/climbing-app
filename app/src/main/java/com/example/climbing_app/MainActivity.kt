c566c4ee607933e7dc54210d251a2f5a506d8778
usetoconfuse <toby.tischler@gmail.com>
2025-05-24 19:03:45 +0100

    Disabled default NavHost fade transitions

--- app/src/main/java/com/example/climbing_app/MainActivity.kt
+++ app/src/main/java/com/example/climbing_app/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -42,7 +44,9 @@ class MainActivity : ComponentActivity() {
                     val navController: NavHostController = rememberNavController()
                     NavHost(
                         navController = navController,
-                        startDestination = AppScreens.Login.name
+                        startDestination = AppScreens.Login.name,
+                        enterTransition = { EnterTransition.None },
+                        exitTransition = { ExitTransition.None }
                     ) {
                         // Login
                         composable(
