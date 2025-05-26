2d8fd9aea79da9d8999bba0d9c84f89640bfe781
usetoconfuse <toby.tischler@gmail.com>
2025-05-26 15:56:42 +0100

    Upload screen now scrolls on page rotation

--- app/src/main/java/com/example/climbing_app/ui/screens/UploadClimbScreen.kt
+++ app/src/main/java/com/example/climbing_app/ui/screens/UploadClimbScreen.kt
@@ -11,7 +11,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
@@ -136,6 +138,7 @@ fun UploadClimbContent(
         modifier = modifier
             .fillMaxSize()
             .padding(16.dp)
+            .verticalScroll(rememberScrollState())
     ) {
         Row(
             modifier = Modifier
