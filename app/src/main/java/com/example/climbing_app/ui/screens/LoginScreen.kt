a02a2ac55f064f093886bfaa18d47f0f57249f84
usetoconfuse <toby.tischler@gmail.com>
2025-05-26 15:57:52 +0100

    Login screen now scrolls on page rotation

--- app/src/main/java/com/example/climbing_app/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/example/climbing_app/ui/screens/LoginScreen.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
@@ -40,6 +42,7 @@ fun LoginScreen(navController: NavController) {
                 .padding(innerPadding)
                 .padding(24.dp)
                 .padding(top = 48.dp)
+                .verticalScroll(rememberScrollState())
         ) {
             val auth = Firebase.auth
             val user = auth.currentUser
